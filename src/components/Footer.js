83a9045b18e664aae16251df9fdd1870e75bc328
LESANF <nagong1000@naver.com>
2022-10-24 03:44:14 +0900

    change myInfo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Footer extends Component {
-  render() {
-    if (this.props.sharedBasicInfo) {
-      var networks = this.props.sharedBasicInfo.social.map(function (network) {
+    render() {
+        if (this.props.sharedBasicInfo) {
+            var networks = this.props.sharedBasicInfo.social.map(function (network) {
+                return (
+                    <span key={network.name} className="m-4">
+                        <a href={network.url} target="_blank" rel="noopener noreferrer">
+                            <i className={network.class}></i>
+                        </a>
+                    </span>
+                );
+            });
+        }
+
         return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
-          </span>
+            <footer>
+                <div className="col-md-12">
+                    <div className="social-links">{networks}</div>
+
+                    <div className="copyright py-4 text-center">
+                        <div className="container">
+                            <small>
+                                Written By &#8211;{' '}
+                                {this.props.sharedBasicInfo ? this.props.sharedBasicInfo.name : '???'}
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         );
-      });
     }
-
-    return (
-      <footer>
-        <div className="col-md-12">
-          <div className="social-links">{networks}</div>
-
-          <div className="copyright py-4 text-center">
-            <div className="container">
-              <small>
-                Copyright &copy;{" "}
-                {this.props.sharedBasicInfo
-                  ? this.props.sharedBasicInfo.name
-                  : "???"}
-              </small>
-            </div>
-          </div>
-        </div>
-      </footer>
-    );
-  }
 }
 
 export default Footer;
